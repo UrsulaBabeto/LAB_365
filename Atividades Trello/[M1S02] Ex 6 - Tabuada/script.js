8cd3af5ef306a943fedd8914953ce9511b048f08
Ursula Babeto <ursulacobabeto@gmail.com>
2023-02-09 14:30:41 -0300

    formatting docs

--- Atividades Trello/[M1S02] Ex 6 - Tabuada/script.js
+++ Atividades Trello/[M1S02] Ex 6 - Tabuada/script.js
@@ -11,18 +11,16 @@ let number = document.getElementById('number');
 let res = document.getElementById('res');
 let total = 0;
 
-button.addEventListener("click", (e) => {
-    for (let i = 1; i < 50; i++) {
-        total = number.value * i;   
 
-        const answerDisplay = document.createElement("p");
-        const text = document.createTextNode(`${number.value} x ${i+1} = ${total} `)
-        answerDisplay.appendChild(text);
-        document.body.insertBefore(answerDisplay, res);
+    button.addEventListener("click", (e) => {
+        for (let i = 1; i < 10; i++) {
 
-     }
-       
-        
-      
- 
-})
+            total = number.value * i;
+
+            //cria dinamicamente uma tag HTML para exibir um conteudo
+            const answerDisplay = document.createElement("p");
+            const text = document.createTextNode(`${number.value} x ${i} = ${total} `)
+            answerDisplay.appendChild(text);
+            document.body.insertBefore(answerDisplay, res);
+        }
+    })
