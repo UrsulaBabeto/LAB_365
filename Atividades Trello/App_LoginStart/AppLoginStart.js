efcdbc3b45144a18105e7db1b664b55f5d494ebe
Ursula Babeto <ursulacobabeto@gmail.com>
2023-02-09 14:46:06 -0300

    minor formatting

--- Atividades Trello/App_LoginStart/AppLoginStart.js
+++ Atividades Trello/App_LoginStart/AppLoginStart.js
@@ -3,29 +3,7 @@ Faça uma página de login que verifique se o login e a senha do usuário batem 
 fictício (armazenados em um objeto). A página deve conter inputs e um botão para logar. Após o login, 
 se as credenciais baterem com as fictícias deve-se imprimir ‘logado’, senão imprimir ‘credenciais 
 incorretas’.
-
-Ex 3 - [App LoginStart] Salvar login no LocalStorage
-Utilizar a página de login para criar um sistema de autenticação utilizando localstorage. Quando o 
-usuário logar, salvar as credenciais localmente. Se as credenciais existirem localmente, ir para uma 
-página com um botão de logout, se elas não existirem, ir para a página de login.
-
-Ex 4 -  [App LoginStart] Operações na conta
-Faça com que o usuário possa alterar informações da conta, que serão salvas em localStorage, como url 
-da imagem de perfil, e-mail e nome.
  */
-
-//EX.2
-/* 
-const identificacao = "UrsulaBabeto";
-const senha = "FloripaTec2023";
-
-var id = prompt('usuario:');
-var password = prompt('Digite sua senha:');
-if (identificacao === id && senha === password){
-    alert('Acesso liberado');
-}else{
-    alert('Usuario ou senha incorretos, tente novamente!')
-} */
 
 let id = document.querySelector("#id");
 let password = document.querySelector("#password");
