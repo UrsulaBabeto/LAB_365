3c575fdbc8857e086fb0a528968747135ed286d3
Ursula Babeto <ursulacobabeto@gmail.com>
2023-02-09 15:02:01 -0300

    teste

---
+++ Atividades Trello/[M1S02] Ex 5 - Cronômetro/script.js
@@ -0,0 +1,6 @@
+/* Crie um cronômetro que seja executado por um botão, conte 10 segundos, e no fim 
+imprima um texto: 'Seu tempo acabou!! Tente novamente!!'. */
+
+for (let i = 0; i <= 10; i++) {
+    console.log(i)
+}
