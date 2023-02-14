97587d3d78c75b2f43b95266d725656820c69e3f
Ursula <ursulacobabeto@gmail.com>
2023-02-13 22:05:52 -0300

    aula 9

---
+++ Semana3/Aula9_Funcoes_de_Array/FuncoesDeArray.js
@@ -0,0 +1,37 @@
+let copa2026 = ['brasil', 'franca', 'alemanha', 'italia', 'espanha']
+copa2026.push('argentina')
+copa2026.unshift('campeche')
+//copa2026.splice(3,0,'Equador')
+copa2026.splice(3, 1, 'equador')
+copa2026.pop()
+
+//cria item html pelo JS
+let list = document.querySelector('#list')
+copa2026.forEach((time) => {
+    let li = document.createElement("li")
+    li.innerText = time
+    list.appendChild(li)
+
+})
+
+//.map()
+const numeros = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+const dobro = numeros.map((numero) => {
+    return numero * 2
+})
+console.log(dobro)
+
+//.filter()
+let numeros1 = [3, 2, 44, 55, 1]
+let numerosMenorQueDez = numeros1.filter((numeros) => {
+    return numeros < 10
+})
+
+let aprovados = ['wallis', 'tales', 'willian', 'glauton']
+let verdadeirosAprovador = aprovados.filter((aluno) => {
+    return aluno !== 'wallis'
+})
+
+//.every()
+const numeros2 = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+console.log(numeros2.every((num) = num < 40))//arrowfunction com retorno explicito pq nao usa chaves
