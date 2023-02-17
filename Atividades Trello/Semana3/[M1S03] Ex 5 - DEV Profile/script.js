0d9dc13d335bb81094784f4b064105d023398276
Ursula <ursulacobabeto@gmail.com>
2023-02-17 13:03:31 -0300

    DEV Profile

--- Atividades Trello/Semana3/[M1S03] Ex 5 - DEV Profile/script.js
+++ Atividades Trello/Semana3/[M1S03] Ex 5 - DEV Profile/script.js
@@ -2,3 +2,23 @@
 seu github.
 Você pode mudar o final do endpoint com seu usuário para obter suas informações.
 https://api.github.com/users/walberson */
+
+let textError = document.querySelector('#textError')
+let userData = document.querySelector('#userData')
+let bio = document.querySelector('#bio')
+let repo = document.querySelector('#repo')
+
+async function findData() {
+    try {
+        const response = await fetch('https://api.github.com/users/UrsulaBabeto');
+        const data = await response.json();
+console.log(data)
+        userData.innerText=data.name;
+        bio.innerText=data.bio;
+        repo.innerText=data.html_url;
+    } catch (e) {
+        textError.innerText = "Erro"
+    }
+}
+
+findData()
