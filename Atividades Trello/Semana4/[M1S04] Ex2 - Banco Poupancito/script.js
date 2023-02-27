2be9fbbc78a71b14b0a7761f9378eec4d2a20fda
Ursula <ursulacobabeto@gmail.com>
2023-02-27 13:02:07 -0300

    'organizing files'

--- Atividades Trello/Semana4/[M1S04] Ex2 - Banco Poupancito/script.js
+++ Atividades Trello/Semana4/[M1S04] Ex2 - Banco Poupancito/script.js
@@ -6,60 +6,10 @@ deposito adiciona valor ao saldo (o usuário deve passar a senha, e ela deve ser
 
 Por fim, instancie um objeto chamado contaCorrente e teste as operações. */
 
- import { Conta } from './conta.js';
-import { Savings } from './savings';
-import { premiumSavings } from './premiumSavings'; 
-class Conta {
-    saldo;
-    #senha;
-    constructor(saldo) {
-        this.saldo = saldo;
-        this.#senha = 1234;
-    }
+import { Conta } from './conta.js';
 
-    get getSenha() {
-        return this.#senha;
-    }
-    get getSaldo() {
-        return this.saldo;
-    }
-    deposito() {
-        let answer = prompt('Valor para deposito: R$')
-        this.saldo += Number(answer)
-        alert(`Saldo atualizado: R$ ${this.saldo}`);
-    }
-    retirada() {
-        let answer = prompt('Valor de saque: R$')
-        this.saldo -= Number(answer);
-        alert(`Saldo atualizado: R$ ${this.saldo}`);
-    }
 
-    mostrarSaldo() {
-       alert(`Saldo em conta: R$ ${this.saldo}`);
-    }
-}
-
-class Savings extends Conta {
-    constructor(saldo, senha) {
-      super(saldo, senha)
-    }
-    atualizaJuros() {
-       alert(`Saldo em conta: R$ ${this.saldo + (this.saldo * 7 / 100)}`);
-    }
-  }
-
-class premiumSavings extends Savings{
-    constructor(saldo, senha) {
-        super(saldo,senha)
-    }
-    atualizaJuros(){
-         alert(`Saldo em conta: R$ ${this.saldo + (this.saldo*1.2/100)}`)
-    }
-  }
-
-const conta = new Conta(30);
-const saving = new Savings(30);
-const pS = new premiumSavings(30);
+const conta = new Conta(30); 
 
 let senha = conta.getSenha;
 
@@ -68,27 +18,19 @@ if (pin == senha) {
 
     let resp1 = true
     while (resp1) {
-        let resp = prompt('Deposito 1;\n Saque 2\n Saldo em conta 3\n Poupança 4\n Poupança Premium 5\n Sair 6')
+
+        let resp = prompt('1 Deposito \n 2 Saque\n 3 Saldo em conta\n 4 Sair ')
         switch (resp) {
             case "1":
-                pS.deposito();
-                //alert(`Saldo atualizado: R$ ${conta.getSaldo}`);
+                conta.deposito();
                 break;
             case "2":
-                pS.retirada();
-                //alert(`Saldo atualizado: R$ ${conta.getSaldo}`);
+                conta.retirada();
                 break;
             case "3":
-                pS.mostrarSaldo()
-                //alert(`Saldo em conta: R$ ${conta.getSaldo}`);
+                conta.mostrarSaldo()
                 break;
             case "4":
-                pS.atualizaJuros();
-                break;
-            case "5":
-                pS.atualizaJuros();
-                break;
-            case "6":
                 resp1 = false;
                 break;
             default:
