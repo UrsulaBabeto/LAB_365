272e964a321715fb0653c1e20087ee0ac113b6c5
Ursula <ursulacobabeto@gmail.com>
2023-03-25 12:10:12 -0300

    exercises 4,5,6

--- AtividadesTrello/Semana8/index.js
+++ AtividadesTrello/Semana8/index.js
@@ -3,12 +3,24 @@
   const user = require("./cadastro.js");
   await database.sync();
 
+  //create
   const newUser = await user.create({
-    firstName: "Ursula",
+    firstName: "juju",
     lastName: "Babeto",
-    cpf: "12345678909",
-    adress: "Rua Mundo de Bob,365",
-    phoneNumber: "999999999",
+    cpf: "12345678911",
+    adress: "",
+    phoneNumber: "",
   });
-  console.log(newUser)
+  console.log(newUser);
+
+  //read
+  const allUsers = await user.findAll(); //find all
+  const userPK = await user.findByPk(3); // find by primary Key
+
+  //update
+  userPK.adress = "Av Allohomora, 934";
+  await userPK.save();
+
+  //delete
+  await userPK.destroy();
 })();
