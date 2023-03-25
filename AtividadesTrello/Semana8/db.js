8315162857a894c6f9acf46b44e74c6a10ebc652
Ursula <ursulacobabeto@gmail.com>
2023-03-25 14:58:48 -0300

    updating

--- AtividadesTrello/Semana8/db.js
+++ AtividadesTrello/Semana8/db.js
@@ -2,7 +2,7 @@ const Sequelize = require('sequelize');
 const sequelize = new Sequelize(
   "Semana8Exercicios",
   "postgres",
-  "SENAI",
+  "*****",
   {
     host: 'localhost',
     port: '5432',
