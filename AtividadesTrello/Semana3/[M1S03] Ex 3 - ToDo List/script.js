0ae1da26048efa5de7bd9bfc14836632f0aea3b0
Ursula <ursulacobabeto@gmail.com>
2023-03-25 21:35:22 -0300

    updating exercises 3th week

--- AtividadesTrello/Semana3/[M1S03] Ex 3 - ToDo List/script.js
+++ AtividadesTrello/Semana3/[M1S03] Ex 3 - ToDo List/script.js
@@ -4,66 +4,47 @@
 03. Função que deleta um item da lista
 04. Função que adiciona um item na lista */
 
-console.log('hello')
+let text = document.querySelector("#text");
+let resposta = document.querySelector("#resposta");
+let list = document.createElement("li");
+
 let resp;
 let arrayTask = [];
 
-while (resp != "5") {
-    let paragraph = prompt('Adicionar tarefa: 1 \nBuscar tarefa: 2 \nAlterar tarefa: 3 \nDeletar tarefa: 4 \nSair: 5')
-
-    switch (paragraph) {
-        case "1":
-            adicionar()
-            break;
-        case "2":
-            buscar()
-            break;
-        case "3":
-            alterar()
-            break;
-        case "4":
-            deletar()
-            break;
-        default:
-            console.log('6')
-            break;
-    }
-}
-
 function adicionar() {
-    let pergunta = prompt('Digite a tarefa a ser adicionada:')
-    arrayTask.push(pergunta)
-    console.log(`Tarefa add: ${arrayTask}`)
+  arrayTask.push(text.value);
+  list.innerText = `Tarefas: \n ${arrayTask}\n`;
+  resposta.appendChild(list);
+  text.value = "";
 }
 
 function buscar() {
-
-    let pergunta = prompt('Digite a tarefa a ser buscada:')
-    let arrayBusca = arrayTask.filter((p) => {
-        return p == pergunta
-    })
-    if (arrayBusca == pergunta) {
-        console.log(`O item: ${arrayBusca} esta na lista`)
-    } else {
-        console.log('não encontrei o item buscado, tente novamente')
-    }
+  let arrayBusca = arrayTask.filter((p) => {
+    return p == text.value;
+  });
+  if (arrayBusca == text.value) {
+    alert(`O item: "${arrayBusca}" ja esta na lista`);
+  } else {
+    alert("não encontrei o item buscado, tente novamente");
+  }
+  text.value = "";
 }
 
 function alterar() {
+  let pergunta = prompt("Digite a tarefa a ser excluida:");
+  let pergunta1 = prompt("Digite a tarefa a ser incluida:");
 
-    let pergunta = prompt('Digite a tarefa a ser excluida:')
-    let pergunta1 = prompt('Digite a tarefa a ser incluida:')
-
-    let index = arrayTask.indexOf(pergunta);
-    arrayTask.splice(index, 1, pergunta1)
-    console.log(`Tarefa add: ${arrayTask}`)
+  let index = arrayTask.indexOf(pergunta);
+  arrayTask.splice(index, 1, pergunta1);
+  list.innerText = `Tarefas:\n ${arrayTask}\n`;
+  text.value = "";
 }
 
 function deletar() {
-    let pergunta = prompt('Digite a tarefa a ser deletada:')
-    let newList = arrayTask.filter((e) => {
-        return e != pergunta
-    })
-    arrayTask = newList
-    console.log(`Tarefa add: ${arrayTask}`)
+  let newList = arrayTask.filter((e) => {
+    return e != text.value;
+  });
+  arrayTask = newList;
+  list.innerText = `Tarefas:\n ${arrayTask}\n`;
+  text.value = "";
 }
